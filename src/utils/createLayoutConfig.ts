71b864059be7275d1786426431f27985a91721f0
Chad Donohue <cdonohue@users.noreply.github.com>
2020-03-31 10:14:26 -0500

    Remove abstractions

--- src/utils/createLayoutConfig.ts
+++ src/utils/createLayoutConfig.ts
@@ -1,8 +1,7 @@
 import createHash from './createHash'
-import useApi from './useApi'
 
 export function enhancePropsWithClassname(
-  props: { [key: string]: any },
+  props: Record<string, any>,
   newClass: string
 ) {
   const { className = '' } = props
@@ -12,57 +11,9 @@ export function enhancePropsWithClassname(
   }
 }
 
-export function reduceApi(name: string, props: { [key: string]: any }) {
-  const api = {
-    ...useApi(name),
-  }
-
-  return {
-    ...Object.keys(api).reduce((apiFinal, key) => {
-      if (props.hasOwnProperty(key)) {
-        const apiValue = props[key]
-        return {
-          ...apiFinal,
-          [key]: apiValue,
-        }
-      }
-
-      return apiFinal
-    }, api),
-  }
-}
-
 export function createLayoutClassname(
-  type: string,
-  api: { [key: string]: any } = {}
+  name: string,
+  props: Record<string, any> = {}
 ) {
-  return `layout-block-${type}-${createHash(JSON.stringify(api))}`
-}
-
-type layoutConfigOptsType = {
-  defaultApi?: any
-  contextApi?: any
-  name: string
-  props?: any
-}
-
-export default function createLayoutConfig(opts: layoutConfigOptsType) {
-  const { name, props } = opts
-
-  const { as: Tag = 'div', children, ...remainingProps } = props
-
-  const api = reduceApi(name, remainingProps)
-
-  Object.keys(api).forEach(key => delete remainingProps[key])
-
-  const layoutClass = createLayoutClassname(name, api)
-  const selector = `${Tag}.${layoutClass}`
-
-  return {
-    api,
-    children,
-    passedProps: enhancePropsWithClassname(remainingProps, layoutClass),
-    selector,
-    Tag,
-  }
+  return `layout-block-${name}-${createHash(JSON.stringify(props))}`
 }
