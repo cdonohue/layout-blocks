a91780e4c8af73f2e69141e87f17eae873974b63
Chad Donohue <cdonohue@users.noreply.github.com>
2020-03-31 10:13:55 -0500

    Simplify scale for now

--- src/utils/LayoutContext.ts
+++ src/utils/LayoutContext.ts
@@ -1,23 +1,8 @@
 import { createContext } from 'react'
-import createScale from './createScale'
 
 const LayoutContext = createContext<Record<string, any>>({
-  api: {
-    box: {
-      padding: 6,
-      size: null,
-      stretch: false,
-    },
-    cover: {
-      height: '100vh',
-    },
-    stack: {
-      gap: '0px',
-      horizontal: false,
-    },
-  },
   theme: {
-    space: createScale(1, 'rem', 1.5),
+    space: (key: number) => `${0.25 * key}rem`,
   },
 })
 
