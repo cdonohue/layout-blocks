871573b28bdfb35560c82573f526c9b62e9b8824
Chad Donohue <cdonohue@users.noreply.github.com>
2020-05-11 09:23:26 -0500

    Cleanup packages

--- src/utils/useApi.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-import LayoutContext from './LayoutContext'
-
-export default function useApi(componentName: string) {
-  const { api } = useContext(LayoutContext)
-  return api[componentName]
-}
--- src/utils/useTheme.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-import LayoutContext from './LayoutContext'
-
-export default function useTheme() {
-  const { theme } = useContext(LayoutContext)
-  return theme
-}
